2556a8491ddb0e7c854819388b51073f39c0538c
Manuel F. Borrego S. <manuelf.borrego@gmail.com>
2025-08-01 15:09:15 -0400

    Actualizando algunos archivos...

---
+++ src/app/pages/counter/counter-page.component.ts
@@ -0,0 +1,21 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  templateUrl: './counter-page.component.html',
+  styleUrls: ['./counter-page.component.css'],
+})
+export class CounterPageComponent {
+  counterSignal = signal(0);
+
+  increaseBy(value: number) {
+    this.counterSignal.update((current) => current + value);
+  }
+
+  decreaseBy(value: number) {
+    this.counterSignal.update((current) => current - value);
+  }
+
+  resetCounter() {
+    this.counterSignal.set(0);
+  }
+}
