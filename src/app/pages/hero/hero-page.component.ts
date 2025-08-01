9e6aae84ede8f5a518cdca64495d1e4fac07e53e
Manuel F. Borrego S. <manuelf.borrego@gmail.com>
2025-08-01 18:28:56 -0400

    Modif. varias...

--- src/app/pages/hero/hero-page.component.ts
+++ src/app/pages/hero/hero-page.component.ts
@@ -1,6 +1,32 @@
 import { Component, signal } from '@angular/core';
 
 @Component({
-  templateUrl:'./hero-page.component.html',
-  //
+  templateUrl: './hero-page.component.html',
+  styleUrls: ['./hero-page.component.css'],
 })
+export class HeroPageComponent {
+  name = signal('Ironman');
+  age = signal(45);
+
+  getHeroDescription() {
+    return `${this.name()}-${this.age()} years old`;
+  }
+
+  changeHero() {
+    this.name.set('Spiderman');
+    this.age.set(25);
+  }
+
+  changeAge() {
+    this.age.set(60);
+  }
+
+  getHeroNameUpperCase() {
+    return `${this.name().toUpperCase()}`;
+  }
+
+  resetHero() {
+    this.name.set('Ironman');
+    this.age.set(45);
+  }
+}
