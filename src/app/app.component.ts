6665ab13a28527c7b44d21e5b809a063c457afc3
Manuel F. Borrego S. <manuelf.borrego@gmail.com>
2025-08-02 04:01:01 -0400

    Creac. 'navbar.component.html' y 'navbar.component.ts'; Modif. varias...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+      selector: 'app-root',
+      imports: [RouterOutlet, NavbarComponent],
+      templateUrl: './app.component.html',
 })
 export class AppComponent {
-  protected readonly title = signal('Manuel F. Borrego S.');
+      protected readonly title = signal('Manuel F. Borrego S.');
 }
