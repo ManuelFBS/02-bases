cdf194acfa8216828fbba8f440215ed14e6a5738
ManuelFBS <manuelf.borrego@gmail.com>
2025-08-03 17:52:19 -0400

    Actualiz. de 'dragonball-page.component.ts'...

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -9,15 +9,10 @@ interface Character {
       templateUrl: './dragonball-page.component.html',
 })
 export class DragonballPageComponent {
-      name = signal('Gohan');
-      power = signal(100);
+      name = signal('');
+      power = signal(0);
 
-      characters = signal<Character[]>([
-            { id: 1, name: 'Goku', power: 9001 },
-            { id: 2, name: 'Vegeta', power: 8000 },
-            { id: 3, name: 'Yamcha', power: 500 },
-            { id: 4, name: 'Piccolo', power: 3000 },
-      ]);
+      characters = signal<Character[]>([{ id: 1, name: 'Goku', power: 9001 }]);
 
       addCharacter() {
             if (!this.name() || !this.power() || this.power() <= 0) {
