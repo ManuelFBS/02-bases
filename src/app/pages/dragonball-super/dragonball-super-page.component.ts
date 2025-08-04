b6244fe1eaa4071e3bc543ca0fa8be34f91aa3c2
ManuelFBS <manuelf.borrego@gmail.com>
2025-08-04 10:33:26 -0400

    Modif. en 'dragonball-super-page.component.html' y 'dragonball-super-page.component.ts'...

--- src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -9,34 +9,16 @@ interface Character {
 }
 @Component({
       templateUrl: './dragonball-super-page.component.html',
+      selector: 'dragonball-super',
       imports: [CharacterListComponent, CharacterAddComponent],
 })
 export class DragonballSuperPageComponent {
-      name = signal('');
-      power = signal(0);
-
       characters = signal<Character[]>([
             { id: 1, name: 'Goku', power: 9001 },
             { id: 2, name: 'Vegeta', power: 8000 },
       ]);
 
-      addCharacter() {
-            if (!this.name() || !this.power() || this.power() <= 0) {
-                  return;
-            }
-
-            const newCharacter: Character = {
-                  id: this.characters().length + 1,
-                  name: this.name(),
-                  power: this.power(),
-            };
-
-            this.characters.update((list) => [...list, newCharacter]);
-            this.resetFields();
-      }
-
-      resetFields() {
-            this.name.set('');
-            this.power.set(0);
+      addCharacter(character: Character) {
+            this.characters.update((list) => [...list, character]);
       }
 }
