c0a62b7b0f77b828c8ab3de1787f167b132a97c2
ManuelFBS <manuelf.borrego@gmail.com>
2025-08-03 22:03:02 -0400

    Creac. de 'character-add.component.html' y 'character-add.component.ts'; Actualiz. de 'dragonball-super-page.component.html' y 'dragonball-super-page.component.ts'...

--- src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { CharacterListComponent } from '../../components/dragonball/character-list/character-list.component';
+import { CharacterAddComponent } from '../../components/dragonball/character-add/character-add.component';
 
 interface Character {
       id: number;
@@ -8,7 +9,7 @@ interface Character {
 }
 @Component({
       templateUrl: './dragonball-super-page.component.html',
-      imports: [CharacterListComponent],
+      imports: [CharacterListComponent, CharacterAddComponent],
 })
 export class DragonballSuperPageComponent {
       name = signal('');
