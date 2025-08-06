51c618f108b78bc5b74c5c89d4afbf8c959462b4
Manuel F. Borrego S. <manuelf.borrego@gmail.com>
2025-08-06 01:12:17 -0400

    Modif. de app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,24 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+      ApplicationConfig,
+      provideBrowserGlobalErrorListeners,
+      provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
-    provideRouter(routes)
-  ]
+      providers: [
+            provideBrowserGlobalErrorListeners(),
+            provideZonelessChangeDetection(),
+
+            provideRouter(routes),
+
+            //? HashStrategy
+            {
+                  provide: LocationStrategy,
+                  useClass: HashLocationStrategy,
+            },
+      ],
 };
